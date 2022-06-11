a9a87bab7f1daa1220789e2bf7de59a678306d9b
fernandedfa <deivisondfaa@gmail.com>
2022-06-11 12:32:02 -0300

    inicio conexao banco de dados

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const config = require('./config');
-
+// const db = require('./db')
 
 // app.use((req, res) => {
 //     res.send("funcionando")
